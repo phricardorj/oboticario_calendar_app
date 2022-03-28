c071806a03dafa70f8609f282ff3b93b480721c7
Pedro Ricardo <70300680+phricardorj@users.noreply.github.com>
2022-03-28 14:21:22 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,6 @@ const weekNow = getWeeksFromDateToToday('2022-01-17');
 const totalWeeks = getWeeksBetweenTwoDates('2022-01-17', '2022-10-17');
 const remainingWeeks = totalWeeks - weekNow;
 
-getElement('#weekNow').innerText = weekNow;
+getElement('#weekNow').innerText = weekNow + 1;
 getElement('#totalWeeks').innerText = totalWeeks;
 getElement('#remainingWeeks').innerText = remainingWeeks;
