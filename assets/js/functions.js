106e748a0cd2696f12bc47af2210e651122e15e9
Pedro Ricardo <70300680+phricardorj@users.noreply.github.com>
2022-05-16 18:36:15 -0300

    feat modal

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -19,5 +19,43 @@ export const getElement = (selector) => {
 };
 
 export const zeroFill = (n) => {
-  return ('0' + n).slice(-2);
+  return ("0" + n).slice(-2);
+};
+
+export const modalOpen = (isOpen) => {
+  modalCreate();
+  const modal = document.querySelector(".modal"),
+    modal_fundo = document.querySelector(".modal_fundo"),
+    close = document.querySelector(".close");
+
+  if (isOpen) {
+    document.body.style.overflow = "hidden";
+    modal.style.visibility = "visible";
+    modal_fundo.style.visibility = "visible";
+  } else {
+    document.body.style.overflow = "visible";
+    modal.style.visibility = "hidden";
+    modal_fundo.style.visibility = "hidden";
+  }
+
+  close.addEventListener("click", () => modalOpen(false));
+  modal_fundo.addEventListener("click", () => modalOpen(false));
+};
+
+/* 
+  ATENÇÃO!! 
+  recomenda-se que você não use o innerHTML, no entanto, 
+  nesta aplicação usarei porque é uma página simples! Mas, cuidado!
+
+  ATTENTION!!
+  it is recommended that you do not use innerHTML, however,
+  in this application I will use it because it is a simple page! But be careful!
+  */
+
+const modalCreate = () => {
+  const n = document.querySelector("#modal"),
+    e = document.createElement("div");
+  (e.innerHTML =
+    '<div class="modal_fundo">\n  <div class="close">❌ Fechar</div>\n</div>\n<div class="modal_container">\n  <div class="modal">\n    <div class="container">\n      <h1>Obrigado pelo seu acesso! 🎉</h1>\n      <p>\n        <strong\n          >Agradeço por todo feedback que recebi em minhas redes!</strong\n        >\n        Sério, isso muda meu dia haha!</p> <p>Constância é mais importante que\n        intensividade e independente da semana atual do Programa, não\n        desanime, certamente, seu resultado será incrível! Fechado? Então... Bons\n        estudos!\n      </p>\n      <p>\n        <i\n          >Nota: O Google Analytics encontra-se ativo nesta aplicação,\n          visando saber como vocês estão encontrando essa página on-line.\n          Combinado? A permanência nessa página, significa que você\n          concordou com essa informação.</i\n        >\n      </p>\n    </div>\n  </div>\n</div>'),
+    n.appendChild(e);
 };
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,31 +3,33 @@ import {
   getWeeksFromDateToToday,
   getElement,
   zeroFill,
-} from './functions.js';
+  modalOpen,
+} from "./functions.js";
 
-const weekNow = getWeeksFromDateToToday('2022-01-17');
-const totalWeeks = getWeeksBetweenTwoDates('2022-01-17', '2022-10-17');
-const remainingWeeks = totalWeeks - (weekNow + 1);
+const weekNow = getWeeksFromDateToToday("2022-01-17"),
+  totalWeeks = getWeeksBetweenTwoDates("2022-01-17", "2022-10-17"),
+  remainingWeeks = totalWeeks - (weekNow + 1);
 
-getElement('#weekNow').innerText = weekNow + 1;
-getElement('#totalWeeks').innerText = totalWeeks;
-getElement('#remainingWeeks').innerText = remainingWeeks;
+getElement("#weekNow").innerText = weekNow + 1;
+getElement("#totalWeeks").innerText = totalWeeks;
+getElement("#remainingWeeks").innerText = remainingWeeks;
 
 setInterval(() => {
-  const now = new Date();
+  const now = new Date(),
+    dataHora =
+      zeroFill(now.getDate()) +
+      "/" +
+      zeroFill(now.getMonth() + 1) +
+      "/" +
+      now.getFullYear() +
+      " " +
+      zeroFill(now.getHours()) +
+      ":" +
+      zeroFill(now.getMinutes()) +
+      ":" +
+      zeroFill(now.getSeconds());
 
-  const dataHora =
-    zeroFill(now.getDate()) +
-    '/' +
-    zeroFill(now.getMonth() + 1) +
-    '/' +
-    now.getFullYear() +
-    ' ' +
-    zeroFill(now.getHours()) +
-    ':' +
-    zeroFill(now.getMinutes()) +
-    ':' +
-    zeroFill(now.getSeconds());
-
-  getElement('#data-hora').innerText = dataHora;
+  getElement("#data-hora").innerText = dataHora;
 }, 1000);
+
+modalOpen(true);
